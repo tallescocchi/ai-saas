26f946116965c1e84374eba9b0ca0249df617def
Talles Cocchi <78833791+tallescocchi@users.noreply.github.com>
2023-07-24 17:49:35 -0300

    Updated Project

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ptBR } from "@clerk/localizations";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={ptBR}>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
